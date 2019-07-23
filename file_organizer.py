756e8b7b3fe6daa053be17a31f0c7ffe030ff6f5
awyn <awynsan@outlook.com>
2019-07-22 20:00:01 -0500

    add tag in the filename; push to pictures dir

--- file_organizer.py
+++ file_organizer.py
@@ -14,85 +14,105 @@ def get_create_timestamp(filename):
 
 def walk_files(path):
     files = []
-    for (dirpath, dirnames, filenames) in os.walk(path):
+    for (dirpath, _, filenames) in os.walk(path):
         for fname in filenames:
             files.append(join(dirpath, fname))
 
 
-def parse_path(path, target_path):
+def parse_path(path, target_path, yr_limit=None, tag=None):
     global fcount
-    print("--organizing " + path + " ...")
-    for (dirpath, dirnames, filenames) in os.walk(path):
+    print("--organizing path=" + path + " ;target_path=" +
+          target_path + ' ;yr_limit=' + str(yr_limit) + ' ;tag=' + tag)
 
+    # if yr_limit:
+    #     target_path = join(target_path, str(yr_limit.year))
+    if not os.path.exists(target_path):
+        os.makedirs(target_path, exist_ok=True)
+
+    for (dirpath, _, filenames) in os.walk(path):
         for fname in filenames:
-            fcount += 1
             src_file = join(dirpath, fname)
 
             # parse filename, separator _;
+            target_dt = None
             try:
-                fprefix, oth = fname.split("_", 1)
-                if len(fprefix) == 8:
-                    datetime_obj = datetime.datetime.strptime(
-                        fprefix, "%Y%m%d")
-                    target_dt = datetime_obj.date()
-                elif fprefix == "IMG" or fprefix == "WP":
-                    fprefix, oth = oth.split("_", 1)
-                    if len(fprefix) == 8:
-                        datetime_obj = datetime.datetime.strptime(
-                            fprefix, "%Y%m%d")
-                        target_dt = datetime_obj.date()
-                else:
-                    target_dt = get_create_timestamp(src_file)
-            except ValueError as identifier:
                 target_dt = get_create_timestamp(src_file)
-            finally:
-                pass
+            except Exception as ex:
+                print("exception {} {}".format(ex, src_file))
+                continue
+
+            if yr_limit:
+                if str(target_dt.year) != yr_limit:
+                    continue
 
             target_dir = join(target_path, target_dt.isoformat())
             # and not os.path.exists(target_dir):
             if not os.path.exists(target_dir):
-                os.mkdir(target_dir)
+                os.makedirs(target_dir, exist_ok=True)
 
             checkFile = join(target_dir, os.path.basename(src_file))
-            print("check file: " + checkFile)
+            # print("check file: " + checkFile)
+
+            if tag:
+                full_target_path = join(
+                    target_dir, tag + '_' + os.path.basename(src_file))
+            else:
+                full_target_path = join(
+                    target_dir, os.path.basename(src_file))
 
             # check if file already exists in target directory, skip
-            if not os.path.exists(join(target_dir, os.path.basename(src_file))):
+            if not os.path.exists(full_target_path):
+                fcount += 1
+
                 try:
-                    print("copying " + src_file + " to " + target_dir)
-                    shutil.copy2(src_file, target_dir)
-                except OSError as identifier:
+                    print("copying " + src_file + " to " + full_target_path)
+                    shutil.copy2(src_file, full_target_path)
+                except Exception as ex:
                     print("Error copying " + src_file + ".  Retrying..")
-                    shutil.copy2(src_file, target_dir)
-                finally:
-                    pass
+                    shutil.copy2(src_file, full_target_path)
             else:
-                print(src_file + " already exists in " + target_dir)
+                print(src_file + " already exists in " + full_target_path)
             print("file count: {} ".format(fcount))
 
             # if fcount >= 10:
-            #    break
+            #     break
 
-# ************
-# main
+        # ************
+        # main
+
+
+def get_tag(path):
+    if 'mobile' in path:
+        return path.split('/')[-1]
+    else:
+        return None
 
 
 fcount = 0
 
-#TARGET_PATH = "/Volumes/pictures/OneDrive_reorg"
-#TARGET_PATH = "/Volumes/library/file_organizer/erwynSE"
+yr_limit = '2017'
+target_path = f'/Volumes/pictures/{yr_limit}/mobile'
 
-# parse_path("/Volumes/pictures/OneDrive/2011")
-# parse_path("/Volumes/pictures/OneDrive/2012")
-# parse_path("/Volumes/pictures/OneDrive/2013")
-# parse_path("/Volumes/pictures/OneDrive/2014")
-# parse_path("/Volumes/pictures/OneDrive/2015")
-# parse_path("/Volumes/pictures/OneDrive/2016")
-# parse_path("/Volumes/pictures/OneDrive/0000_UnknownDate")
+source_path = '/Volumes/library/mobile/agnes-x'
+tag = get_tag(source_path)
+parse_path(source_path,
+           target_path=target_path,
+           yr_limit=yr_limit,
+           tag=tag)
 
-parse_path("/Volumes/library/mobile/erwynSE",
-           "/Volumes/library/file_organizer/erwynSE")
+source_path = '/Volumes/library/mobile/sofia'
+tag = get_tag(source_path)
+parse_path(source_path,
+           target_path=target_path,
+           yr_limit=yr_limit,
+           tag=tag)
 
+source_path = '/Volumes/library/mobile/erwynSE'
+tag = get_tag(source_path)
+parse_path(source_path,
+           target_path=target_path,
+           yr_limit=yr_limit,
+           tag=tag)
 
 print("copied {} files".format(fcount))
 
